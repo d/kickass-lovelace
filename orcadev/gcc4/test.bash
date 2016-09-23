3b953dafb5acee856187d9090923e1d656530af2
Jesse Zhang <sbjesse@gmail.com>
2016-09-22 22:11:24 -0700

    Add test for GCC4 image

---
+++ orcadev/gcc4/test.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+_main() {
+	it_has_gcc4
+
+	it_has_modern_cmake
+
+	it_has_python27
+
+	it_has_executables
+}
+
+it_has_gcc4() {
+	gcc --version | fgrep --quiet 4.9.4
+	g++ --version | fgrep --quiet 4.9.4
+}
+
+it_has_modern_cmake() {
+	cmake --version | fgrep --quiet 3.6.1
+}
+
+it_has_python27() {
+	[[ "$(python --version 2>&1)" == Python\ 2.7.12 ]]
+}
+
+it_has_executables() {
+	local EXECUTABLES=(
+		gpg
+		vim
+		make
+		patch
+		bzip2
+		xz
+		unzip
+		p7zip
+		pigz
+		wget
+	)
+}
+
+_main "$@"
