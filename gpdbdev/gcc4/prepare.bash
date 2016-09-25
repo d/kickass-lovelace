5ddbccab2db48c418e6b2d7e7c406691ff1f4376
Jesse Zhang <sbjesse@gmail.com>
2016-09-24 17:50:39 -0700

    First stab at adding GCC 4.9.4 to CentOS 6

---
+++ gpdbdev/gcc4/prepare.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+_main() {
+	local DIRECTORIES=(
+		src/orcadev/gcc4/
+		cmake
+		gcc4
+		ccache
+	)
+	unzip ninja/ninja-linux.zip -d prepared
+	rsync -a "${DIRECTORIES[@]}" prepared
+}
+
+_main "$@"
---
+++ gpdbdev/gcc4/test.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+_main() {
+	it_has_gcc4
+
+	it_has_modern_cmake
+
+	it_has_modern_ccache
+
+	it_has_ninja_17
+
+	it_has_executables
+}
+
+it_has_gcc4() {
+	gcc --version | fgrep --quiet 4.9.4
+	g++ --version | fgrep --quiet 4.9.4
+}
+
+it_has_libc_objects() {
+	(
+	set -e
+	pushd "$(mktemp -d -t simple_compilation.XXX)"
+	cat > hello.c <<HELLO
+int main() { return 0; }
+HELLO
+	gcc -o hello hello.c
+	)
+}
+
+it_has_modern_cmake() {
+	cmake --version | fgrep --quiet 3.6.1
+}
+
+it_has_modern_ccache() {
+	ccache --version | fgrep --quiet 'ccache version 3.3.1'
+}
+
+it_has_ninja_17() {
+	[ "$(ninja --version)" = "1.7.1" ]
+}
+
+it_has_executables() {
+	local EXECUTABLES=(
+		vim
+		make
+		patch
+		bzip2
+		xz
+		unzip
+		7za
+		pigz
+		wget
+	)
+	for executable in "${EXECUTABLES[@]}"; do
+		type -p "${executable}"
+	done
+}
+
+_main "$@"
