28f043ab9d5246e1537dd8e27652dc69782e6afa
Jesse Zhang <sbjesse@gmail.com>
2016-10-09 18:00:28 -0700

    Use xz instead of gzip

--- gcc/compile.bash
+++ gcc/compile.bash
@@ -25,7 +25,9 @@ build_gcc() {
 }
 
 copy_output() {
-	tar cf gcc/gcc.tar.gz --use-compress-program pigz -C staging .
+	# in newer versions of tar we would be able to use
+	# --use-compress-program 'xz -T 0 --best'
+	env XZ_OPT='-T 0 --best' tar cf gcc/gcc.tar.xz --use-compress-program xz -C staging .
 }
 
 _main "$@"
